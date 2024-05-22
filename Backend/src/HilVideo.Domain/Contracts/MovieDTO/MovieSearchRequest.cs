fc80d1e80713ae3561b1d33f4167d688271aa48a
HILTON <artyomhilton@yandex.ru>
2024-05-23 00:30:24 +0300

    Backend: Fix GetSearchMovies API

--- Backend/src/HilVideo.Domain/Contracts/MovieDTO/MovieSearchRequest.cs
+++ Backend/src/HilVideo.Domain/Contracts/MovieDTO/MovieSearchRequest.cs
@@ -11,23 +11,29 @@ public record MovieSearchRequest(string MovieName, List<Guid>? Genres, int SortB
         {
             return false;
         }
-        
+    
         try
         {
-            var parsedUrl = url.Split('?')[1];
-            var paramsCollections = HttpUtility.ParseQueryString(parsedUrl);
+            var parsedUrl = url.Split('?');
+            var paramsCollections = HttpUtility.ParseQueryString(parsedUrl.Length > 1 ? parsedUrl[1] : "");
 
+            // Извлекаем значения параметров из строки 
             var movieName = paramsCollections["MovieName"];
             var genres = paramsCollections["Genres"]?.Split(',')
                 .Where(x => !string.IsNullOrEmpty(x) && !string.IsNullOrWhiteSpace(x)).Select(Guid.Parse).ToList();
-            var sortBy = Convert.ToInt16(paramsCollections["SortBy"]);
+            var sortBy = paramsCollections["SortBy"];
             
-            result = new MovieSearchRequest(movieName, genres, sortBy);
+            int? sortByInt = !string.IsNullOrEmpty(sortBy) ? Convert.ToInt32(sortBy) : null;
+        
+            // Создаем объект
+            result = new MovieSearchRequest(movieName, genres, sortByInt ?? default);
             return true;
         }
-        catch
+        catch(Exception e)
         {
+            Console.WriteLine(e);
             return false;
         }
     }
+
 }
